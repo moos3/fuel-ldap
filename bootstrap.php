53aaba6f795b7956622d4f3108b1be2236de9305
moos3 <richard@guthnur.net>
2011-08-15 22:24:54 -0400

    fix to the bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,5 @@
 Autoloader::add_core_namespace('Ldap');
 Autoloader::add_classes(array(
 	'Ldap\\Ldap' => __DIR__.'/classes/ldap.php',
-	'Ldap\\Auth' => __DIR__.'/classes/auth_ldap.php'
+	'Ldap\\Auth_Ldap' => __DIR__.'/classes/auth_ldap.php'
 ));
